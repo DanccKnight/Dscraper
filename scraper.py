cf6e906317c075c350479cebe399c77757e83974
Ansuman Acharya <nitinansu12@gmail.com>
2020-03-18 20:19:51 +0530

    Fetch and store links of chapters

--- scraper.py
+++ scraper.py
@@ -5,7 +5,12 @@ url = 'https://ww5.readmha.com/manga/boku-no-hero-academia/'
 
 r = requests.get(url)
 soup = BeautifulSoup(r.text,'html.parser')
-links = soup.find_all('a',class_="text-gray-900 dark:text-white text-lg font-semibold mb-1")
+links_set = soup.find_all('a',class_="text-gray-900 dark:text-white text-lg font-semibold mb-1")
+
+links = []
+
+for link in links_set:
+    links.append(str(link['href']))
 
 for link in links:
-    print(link['href'])
+    print(link)
