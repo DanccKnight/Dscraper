6095925f8bb9527507aa59920fd99a2559bf939f
Ansuman Acharya <nitinansu12@gmail.com>
2020-03-19 23:45:37 +0530

    Differentiated chapters that have content and chapters that don't

--- scraper.py
+++ scraper.py
@@ -14,7 +14,6 @@ soup = BeautifulSoup(r.text,'html.parser')
 links_set = soup.find_all('a', class_ = "text-gray-900 dark:text-white text-lg font-semibold mb-1")
 
 chapter_links = []
-image_links = []
 
 for link in links_set:
     chapter_links.append(str(link['href']))
@@ -22,16 +21,22 @@ for link in links_set:
 var = len(chapter_links) + 1
 
 for link in chapter_links:
-    print("Chapter ->" + link)
+    image_links = []
     web_url = link
     r = requests.get(web_url)
     soup = BeautifulSoup(r.text,'html.parser')
     img_set = soup.find_all('img', class_ = "my-3 mx-auto js-page")
+    
     for x in img_set:
         image_links.append(str(x['src']))
-    print("Image ->")
+   
+    if (len(image_links) < 3):
+        continue
+    print("Number of image links fetched:" + str(len(image_links)))
+    print("Chapter link:- " + link)
+    print("Image links:-")
     for x in image_links:
         print(x)
-
+    print("")
 #    print(str(var) + ":" + link)
 #    db.collection('My Hero Academia').document('Chapter ' + str(var)).
