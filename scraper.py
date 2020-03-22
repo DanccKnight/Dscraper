b82ba47c487deddc6c824c691081199eeb5ec4af
Ansuman Acharya <nitinansu12@gmail.com>
2020-03-22 20:17:39 +0530

    Upload image links to database

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import firebase_admin
 from firebase_admin import credentials, firestore
+import time
 
 cred = credentials.Certificate("/home/ansuman/scrape/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
@@ -21,27 +22,39 @@ for link in links_set:
     chapter_text.append(link.text)
 
 for link in chapter_links:
-    image_links = []
-    web_url = link
-    r = requests.get(web_url)
-    soup = BeautifulSoup(r.text,'html.parser')
-    img_set = soup.find_all('img', class_ = "my-3 mx-auto js-page")
+    try:
+        image_links = []
+        web_url = link
+        r = requests.get(web_url)
+        soup = BeautifulSoup(r.text,'html.parser')
+        img_set = soup.find_all('img', class_ = "my-3 mx-auto js-page")
     
-    for x in img_set:
-        content = str(x['src'])
-        if(content[len(content)-1] == '0'):
-            continue
-        image_links.append(str(x['src']))
+        for x in img_set:
+            content = str(x['src'])
+            if(content[len(content)-1] == '0'):
+                continue
+            image_links.append(str(x['src']))
    
-    if (len(image_links) < 3):
-        continue
-    print("Chapter Name:- " + chapter_text[chapter_links.index(link)])
-    print("Chapter link:- " + link)
-    print("Number of image links fetched:" + str(len(image_links)))
-    print("Image links:-")   
+        if (len(image_links) < 3):
+            continue
+        index = chapter_links.index(link)
+        print("Chapter Name:- " + chapter_text[index])
+        print("Chapter link:- " + link)
+        print("Number of image links fetched:" + str(len(image_links)))
+        print("Image links:-")   
+        
+        for x in image_links:
+            print(x)
+        print("")
+        
+        data = {
+            u'images': image_links   
+        }
     
-    for x in image_links:
-        print(x)
-    print("")
+        db.collection(u'My Hero Academia').document(chapter_text[index]).set(data)
+        print("Suspending for 3 seconds...")
+        time.sleep(3)
+    except:
+        print("Something went wrong :(")
 
-#    db.collection('My Hero Academia').document()
+print("Uploaded all chapters successfully!")
