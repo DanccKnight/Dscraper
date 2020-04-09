7c03a10e3eddc05e07bcd8b025ab3e448f88be05
Ansuman Acharya <nitinansu12@gmail.com>
2020-04-09 21:50:38 +0530

    Updated url

--- individualInsert.py
+++ individualInsert.py
@@ -7,18 +7,19 @@ from firebase_admin import credentials, firestore
 cred = credentials.Certificate("/home/ansuman/scrape/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-num = 220
-web_url = 'https://ww5.readmha.com/manga/boku-no-hero-academia-chapter-' + str(num)
+num = 265
+web_url = 'https://ww5.readmha.com/chapter/boku-no-hero-academia-chapter-' + str(num)
 
 r = requests.get(web_url)
 soup = BeautifulSoup(r.text,'html.parser')
 img_links = []
 
 for i in soup.find_all('img', class_ = "my-3 mx-auto js-page"):
-    content = str[i['src']]
-    if(content[len(content)-1] == '0'):
-        continue
+    content = str(i['src'])
+    #if(content[len(content)-1] == '0'):
+    #    continue
     img_links.append(content)
+    print(content)
 
 data = {
     'number': num,
